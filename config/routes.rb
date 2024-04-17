26f5eeaf594cf799c54d9a2ed20314ee16830c16
zakaali <zakaali261111@gmail.com>
2024-04-17 23:07:32 +0530

    navbar setup, added new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :home do
+    get :generate, on: :collection
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
